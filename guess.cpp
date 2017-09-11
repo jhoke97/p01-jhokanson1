085713183c7ef2a7a768d113662972503399bd15
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-09-11 10:49:11 -0700

    Now includes difficulties

--- guess.cpp
+++ guess.cpp
@@ -5,11 +5,29 @@
 using namespace std;
 
 int main(){
-int cpuNum;
-int userGuess; 
+  int cpuNum;
+  int userGuess; 
+  string difficulty; 
+  string Hard = "H";
+  string Medium = "M";
+  string Easy = "E";
+  int maxNum;
 srand (time(NULL));
-cpuNum = rand() % 10 +1; 
-cout << "Guess a random number between 1 and 10" << cpuNum << endl;
+cout << "Welcome to the guessing game. Which difficulty would you like?" << endl << "Easy = E" << endl << "Medium = M" << endl << "Hard = H" << endl;
+cin >> difficulty;
+if(difficulty =="E"){
+  cpuNum = rand() % 2 + 1;
+  maxNum = 2;
+}
+if(difficulty =="M"){
+  cpuNum = rand() % 10 + 1;
+maxNum = 10;
+}
+if(difficulty =="H"){
+  cpuNum = rand() % 100 +1;
+  maxNum = 100;
+}
+cout << "Guess a random number between 1 and " << maxNum << cpuNum << endl;
 cin >> userGuess;
 if(userGuess == cpuNum){
   cout << "You guessed correctly, you win!" << endl;
