118ed90839c2ccc68b9fdd0228427e6f7e7ee9c1
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-09-13 12:13:55 -0700

    Includes difficulty, displays higher or lower guess, loops infinitely

--- guess.cpp
+++ guess.cpp
@@ -12,8 +12,11 @@ int main(){
   string Medium = "M";
   string Easy = "E";
   int maxNum;
-srand (time(NULL));
-cout << "Welcome to the guessing game. Which difficulty would you like?" << endl << "Easy = E" << endl << "Medium = M" << endl << "Hard = H" << endl;
+  srand (time(NULL));
+  bool playAgain=true;
+  string playInput;
+  while(playAgain==true){
+  cout << "Welcome to the guessing game. Which difficulty would you like?" << endl << "Easy = E" << endl << "Medium = M" << endl << "Hard = H" << endl;
 cin >> difficulty;
 if(difficulty =="E"){
   cpuNum = rand() % 2 + 1;
@@ -27,15 +30,17 @@ if(difficulty =="H"){
   cpuNum = rand() % 100 +1;
   maxNum = 100;
 }
-cout << "Guess a random number between 1 and " << maxNum << cpuNum << endl;
+cout << "Guess a random number between 1 and " << maxNum << endl;
 cin >> userGuess;
 if(userGuess == cpuNum){
   cout << "You guessed correctly, you win!" << endl;
 }else{
   if(cpuNum > userGuess){
-    cout << "You were off by " << cpuNum - userGuess << " Sorry, you lose" << endl;
+    cout << "You were " << cpuNum - userGuess << " lower than the correct number. Sorry, you lose" << endl;
   }else{
-    cout << "You were off by " << userGuess - cpuNum << " Sorry, you lose" << endl;
+    cout << "You were " << userGuess - cpuNum << " higher than the correct number. Sorry, you lose" << endl;
   }
      }
+  cout << "Would you like to play again?" << endl;
+}
 }
